7b5e8e9b79137393d987255f1c53e418c3946cbc
F3YoD <y.djama@hotmail.fr>
2018-03-09 10:02:41 +0100

    V.1

---
+++ src/Carte.java
@@ -0,0 +1,14 @@
+public class Carte {
+    private String nom;
+    private int pv;
+    private int cout;
+    private int degats;
+
+    //setter
+    public Carte(){
+        nom = null;
+        pv=0;
+        cout=0;
+        degats=0;
+    }
+}
