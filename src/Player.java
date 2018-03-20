b45c6eeef5deff17e2ab6c5961ba79db0d495fb5
paq1 <p.paquin1016@laposte.net>
2018-03-20 14:09:34 +0100

    commit player ....

--- src/Player.java
+++ src/Player.java
@@ -1,3 +1,13 @@
 public class Player {
     private int vies;
+    private Main main;
+
+    public Player(int pV, Main pMain){
+        this.vies = pV;
+        this.main = pMain;
+    }
+
+    public String toString(){
+        return "PV : " + this.vies + "\n";
+    }
 }
