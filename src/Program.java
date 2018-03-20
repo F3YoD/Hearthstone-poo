df091d2cc37fdf7edb3f83ab083cbd745ebe70e4
paq1 <p.paquin1016@laposte.net>
2018-03-20 14:07:51 +0100

    test programme principal : pierre

---
+++ src/Program.java
@@ -0,0 +1,8 @@
+public class Program {
+
+    public static void main(String argv[]){
+        // declaration des objets
+        Player player1 = new Player(12, new Main());
+        System.out.println("" + player1.toString());
+    }
+}
