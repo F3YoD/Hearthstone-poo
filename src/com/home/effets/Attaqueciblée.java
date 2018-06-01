cb76f9e5cf6fb2f6733d2b238f94daa19c9335a7
F3YoD <y.djama@hotmail.fr>
2018-06-01 15:04:53 +0200

    Debut gestion mana

--- src/com/home/effets/Attaqueciblée.java
+++ src/com/home/effets/Attaqueciblée.java
@@ -2,6 +2,8 @@ package com.home.effets;
 
 import com.home.Environement.Plateau;
 import com.home.carte.Carte;
+import com.home.exception.mauvaisIdException;
+import com.home.exception.noCarteException;
 
 import java.util.Scanner;
 
@@ -35,7 +37,23 @@ public class Attaqueciblée extends Capacite {
         Scanner sc = new Scanner(System.in);
         System.out.println("Qui voulez vous attaquer ? (Si vous voulez attaquer le Héro adverse marquez hero sinon le nom de la carte \n) ");
         String cherche =sc.nextLine();
-
+        if (cherche=="hero"){
+             try {
+                 plat.joueurAAttaquer(idLanceur).getHero().addVies(-this.degats);
+             }catch (mauvaisIdException e){
+                 //TODO gerer l'erreur
+             }
+        }
+        else{
+            try{
+                plat.joueurAAttaquer(idLanceur).getTerrain().Findwithname(cherche).addVies(-this.degats);
+            }catch (noCarteException e){
+                System.out.println("La carte n'est pas présente en face");
+                this.realiser(lanceur,plat,idLanceur);
+            }catch (mauvaisIdException e1){
+                //TODO gerer l'erreur
+            }
+        }
 
     }
 
