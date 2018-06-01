0f2dfcdf799a4366baa77406aa50a82a5721955c
paq1 <p.paquin1016@laposte.net>
2018-06-01 21:58:09 +0200

    changement de capacite class --> interface

--- src/com/home/application/Program.java
+++ src/com/home/application/Program.java
@@ -58,24 +58,69 @@ public class Program {
         Serviteur c10 = new Serviteur("Gnôme lépreux", 1,1,1,new AttaqueMentale(2));
         Serviteur c11 = new Serviteur("Golem des moissons", 3,3,2,new Invocation("Golemisation",1,2,1));
 
-        pDeck.add(c1);
-        pDeck.add(c2);
-        pDeck.add(c3);
-        pDeck.add(c4);
-        pDeck.add(c5);
-        pDeck.add(c6);
+        // carte specifique a Rexar
+        Serviteur c12 = new Serviteur("Busard affamé",5,2,3,new Pioche(1));
+        Sort c13 = new Sort("Marque du chasseur", 1,new MarqueDuChasseur());
+        Sort c14 = new Sort("Tir des arcanes", 1,new AttaqueMentale(2));
+        Sort c15 = new Sort("Lâchez les chiens",3,new Invocation("Lâchez les chiens",1,1,3));
+        Sort c16 = new Sort("Ordre de tuer",3,new Attaqueciblée("Ordre de tuer", 3));
+
+        pDeck.add( c1);
+        pDeck.add( c2);
+        pDeck.add( c3);
+        pDeck.add( c4);
+        pDeck.add( c5);
+        pDeck.add( c6);
+        pDeck.add( c7);
+        pDeck.add( c8);
+        pDeck.add( c9);
+        pDeck.add(c10);
+        pDeck.add(c11);
+        pDeck.add(c12);
+        pDeck.add(c13);
+        pDeck.add(c14);
+        pDeck.add(c15);
+        pDeck.add(c16);
 
     }
 
     public static void InitDeckJaina(Deck pDeck){
         Serviteur c1 = new Serviteur("Chasse-marrée murloc", 2, 1, 2, new Invocation("cri de guerre", 1, 1, 1));
         Sort c2 = new Sort("Charge", 1,new Charge());
-        Serviteur c3 = new Serviteur("Champion de hurlevent",7,6,6);
-        Serviteur c4= new Serviteur("Gnome lepreux",1,1,1,new Attaqueciblée("Attaque du lepreux",2));
+        Sort c3 = new Sort("Attaque Mentale",2,new AttaqueMentale(5));
+        Serviteur c4 = new Serviteur("Champion de hurlevent",7,6,6,
+                new CapaciteBonus("Bonus de Hurlevent",1,1));
+        Serviteur c5 = new Serviteur("Chef de raid",3,2,2, new CapaciteBonus("Bonus du chef de guerre",1,0));
+        Serviteur c6 = new Serviteur("Garde de Baie-du-butin",5,4,5,new Provocation());
+        Serviteur c7 = new Serviteur("La missilière téméraire",6,2,5,new Charge());
+        Serviteur c8 = new Serviteur("L'ogre-magi",4,4,4,new Provocation());
+        Serviteur c9 = new Serviteur("Archimage",6,7,4,new Provocation());
+        Serviteur c10 = new Serviteur("Gnôme lépreux", 1,1,1,new AttaqueMentale(2));
+        Serviteur c11 = new Serviteur("Golem des moissons", 3,3,2,new Invocation("Golemisation",1,2,1));
 
-        pDeck.add(c1);
-        pDeck.add(c2);
-        pDeck.add(c3);
+        // carte specifique a Jaina
+        Sort c12 = new Sort("Choc de flamme",7,new CapaciteDegatMassive(4));
+        Sort c13 = new Sort("Eclair de givre", 2, new Attaqueciblée("Attaque du givre",3));
+        Sort c14 = new Sort("Intelligence des arcanes", 2,new Pioche(2));
+        Sort c15 = new Sort("Image mirroir", 1, new Invocation("Image Mirroir", 2,0,2)); // todo provocation + invoc
+        Sort c16 = new Sort("Explosion pyrotechnique", 10,new Attaqueciblée("Explosion pyrotechnique",10));
+
+        pDeck.add( c1);
+        pDeck.add( c2);
+        pDeck.add( c3);
+        pDeck.add( c4);
+        pDeck.add( c5);
+        pDeck.add( c6);
+        pDeck.add( c7);
+        pDeck.add( c8);
+        pDeck.add( c9);
+        pDeck.add(c10);
+        pDeck.add(c11);
+        pDeck.add(c12);
+        pDeck.add(c13);
+        pDeck.add(c14);
+        pDeck.add(c15);
+        pDeck.add(c16);
     }
 
     public static void startPartie(Plateau p) throws mauvaisIdException {
